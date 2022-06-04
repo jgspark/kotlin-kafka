5d8e2dca1b14cc4a78faddd95947d739c8dee07c
noveltaker <pnw123456@icloud.com>
2022-06-04 19:14:02 +0900

    chore: package name change

--- src/main/kotlin/com/example/kotlinkafka/config/ProducerConfiguration.kt
+++ src/main/kotlin/com/example/kotlinkafka/config/ProducerConfiguration.kt
@@ -1,4 +1,4 @@
-package com.example.kotlinkafka.confifg
+package com.example.kotlinkafka.config
 
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
