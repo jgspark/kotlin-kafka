ec3f27eba81442c80662f3f96d530ca6a89edbcc
noveltaker <pnw123456@icloud.com>
2022-06-07 23:04:16 +0900

    chore: change the time data type form long to date in metadata

--- src/main/kotlin/com/example/kotlinkafka/consumer/TestConsumer.kt
+++ src/main/kotlin/com/example/kotlinkafka/consumer/TestConsumer.kt
@@ -7,6 +7,7 @@ import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
+import java.util.Date
 import java.util.concurrent.CountDownLatch
 
 @Component
@@ -26,7 +27,8 @@ class TestConsumer {
         @Header(name = KafkaHeaders.RECEIVED_MESSAGE_KEY, required = false) key: String,
         @Header(KafkaHeaders.RECEIVED_PARTITION_ID) partition: Int,
         @Header(KafkaHeaders.RECEIVED_TOPIC) topic: String,
-        @Header(KafkaHeaders.RECEIVED_TIMESTAMP) time: Long
+        // time 의 경우 long type 과 date type 둘다 사용가능하다.
+        @Header(KafkaHeaders.RECEIVED_TIMESTAMP) time: Date
     ) {
         payload = message
         println("payload : ${message} \n key : ${key} \n partition : ${partition} \n topic : ${topic} \n time : ${time}")
