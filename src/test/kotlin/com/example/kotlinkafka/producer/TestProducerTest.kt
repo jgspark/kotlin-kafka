14861eb163307cd3f550b5adcc33eda340ba70b2
noveltaker <pnw123456@icloud.com>
2022-06-19 14:44:59 +0900

    test: send data null test case add

--- src/test/kotlin/com/example/kotlinkafka/producer/TestProducerTest.kt
+++ src/test/kotlin/com/example/kotlinkafka/producer/TestProducerTest.kt
@@ -1,8 +1,10 @@
 package com.example.kotlinkafka.producer
 
+import com.example.kotlinkafka.exception.NoSendDataException
 import com.example.kotlinkafka.producer.dto.TestDTO
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.test.context.EmbeddedKafka
@@ -25,5 +27,14 @@ internal class TestProducerTest {
         val dto = TestDTO("test100")
         val data = testProducer?.save(dto)
         Assertions.assertEquals(dto.getName(), data?.getName())
+    }
+
+    @Test
+    fun `카프카에서 메세지를 null 로 던지는 케이스`() {
+        Assertions.assertThrows(NoSendDataException::class.java,
+            Executable {
+                testProducer?.save(null)
+            }
+        )
     }
 }
